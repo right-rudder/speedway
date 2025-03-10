61fe4baa2ed9404a4592666b6d50b9f7b3c85937
lucascr <lcrodrigues1995@gmail.com>
2025-03-10 12:29:43 -0300

    Update tailwind.config.mjs

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,23 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				accent: {
+					'50': '#fef2f2',
+					'100': '#fee2e2',
+					'200': '#fecaca',
+					'300': '#fca5a5',
+					'400': '#f87171',
+					'500': '#ef4444',
+					'600': '#dc2626',
+					'700': '#b91c1c',
+					'800': '#991b1b',
+					'900': '#7f1d1d',
+					'950': '#450a0a',
+				}
+			}
+		},
 	},
 	plugins: [],
 }
