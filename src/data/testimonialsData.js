71dbf69c6f431e6473b28d134579b4e72acbea1c
lucascr <lcrodrigues1995@gmail.com>
2025-03-12 14:52:17 -0300

    Create testimonialsData.js

---
+++ src/data/testimonialsData.js
@@ -0,0 +1,57 @@
+const testimonialsData = {
+  upperHeading: "The Speedway Experience",
+  heading: "What Our Students Are Saying",
+  headingText: [
+    "At Speedway Flight Training, we pride ourselves on offering top-notch training with a personal touch. Our goal is to create confident, capable pilots while fostering a professional yet welcoming atmosphere.",
+    "Check out what our students and visitors have to say about their journey with Speedway!"
+  ],
+  buttonText: "Share Your Experience",
+  buttonLink:
+    "https://search.google.com/local/writereview?placeid=ChIJZyqAXVebwoARFSs_Fz2HHeA",
+  reviews: [
+    {
+      body: `Just passed my checkride with a great experience ! I work here at the airport and know what goes on and LA flight academy is the best school here.  Well maintained planes, friendly and caring instructors , HONEST business tactics, owners and just a great community and vibe. Many decent schools here but this is THE place my peers and I would recommend.`,
+      author: "Jonathan D."
+    },
+    {
+      body: `Amazing Service, Fantastic Instructors, and Great Aircraft!
+
+        I recently had the pleasure of flying with Speedway Flight Training, and I can’t say enough good things about my experience. From start to finish, the service was top-notch. The staff were incredibly friendly and accommodating, making the entire process smooth and enjoyable.
+
+        The instructors are fantastic—knowledgeable and truly passionate about flying. Their expertise and enthusiasm are truly inspiring.
+
+        The aircraft are well-maintained and in excellent condition. I had the opportunity to fly a Baron BE55, and it was an absolute joy.
+
+        The views during our flight to Catalina Island were breathtaking, especially the south coastal transition over LAX. The landing at the Airport in the Sky was an unforgettable experience.
+        `,
+      author: "Kaine S."
+    },
+    {
+      body: `I seriously can't recommend Speedway Flight Training enough! I'm a career student here and the instructors, planes, and staff are the best I've come across since I started flying. If you are thinking about getting your pilot's license for fun or flying as a career you should definitely make Speedway Flight Training your first stop when looking for a school!`,
+      author: "Henry S."
+    },
+    {
+      body: `The best flight school in Georgia.
+      I took here my Instrument and commercial.
+      Amazing instructors, the planes in very good condition. Nice atmosphere.
+      Definitely recommend!`,
+      author: "Shlomi E."
+    },
+    {
+      body: `If you are looking for somewhere to learn to fly, seriously look no further. After coming from a not so good flight school, Speedway Flight Training took me in and turned me into a really confident safe pilot. I went from student pilot through to CFi with them and I wouldn't have it any other way. The aircraft are always maintained well and are safe, the flight instructors are professional and their only goal it to have you succeed. The business is very transparent and the owner is extremely accommodating and it is a very friendly Environment place to come and learn. I can't recommend this place enough, go see for yourself!`,
+      author: "Matthew R."
+    },
+    {
+      body: `I completed my commercial multi engine  rating at this school and had the best experience here . The first day I enrolled the owner of the school got me a check ride date on the spot ; the owner is very responsive , kind and cares about the progress of students and CFIs .
+My instructor was Alex feldman, he’s very knowledgeable and makes the learning process extremely easy and straightforward. My instructor made sure I was very proficient in my knowledge and flying before he signed me off for the Checkride .My training took less than a week to complete .
+Overall this school has a very great culture where CFIs care a lot about the progress of their students . The owner also hosts holidays/events and brings people together which I think is very awesome.`,
+      author: "Ajuma O."
+    },
+    {
+      body: `Speedway Flight Training is a great flight school with great instructors and team of employees. Recently got checked out to rent with them and they were one of the only flight schools in the area that were reasonable with checkout flight requirements. In the short time flying with them, their commitment to excellent training and flight instruction was evident, and I wouldn’t hesitate to fly with any of their instructors again.`,
+      author: "Boaz F."
+    }
+  ]
+};
+
+export default testimonialsData;
