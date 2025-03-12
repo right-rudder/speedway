a2e7f089f1a55501bf084649449a75b1e8af80e4
lucascr <lcrodrigues1995@gmail.com>
2025-03-12 14:48:48 -0300

    Create programs.js

---
+++ src/data/programs.js
@@ -0,0 +1,58 @@
+const programs = {
+  upperHeading: "Explore",
+  heading: "Our Training Programs",
+  description:
+    "Whether you're starting your aviation journey, advancing your skills, or pursuing a career in aviation, Speedway Flight Training offers tailored programs for all experience levels.",
+  cards: [
+    {
+      title: "Private Pilot",
+      imagePath: "/src/assets/placeholder.jpg",
+      imageAlt: "Photograph of an Speedway training aircraft on the runway",
+      description:
+        "Take the first step toward becoming a pilot with our comprehensive Private Pilot Course.",
+      link: "/programs/private-pilot-course",
+    },
+    {
+      title: "Instrument Rating",
+      imagePath: "/src/assets/placeholder.jpg",
+      imageAlt: "Close-up of Speedway aircraft instruments",
+      description:
+        "Enhance your skills and safely navigate through low-visibility conditions with our Instrument Rating course.",
+      link: "/programs/instrument-rating",
+    },
+    {
+      title: "Commercial Pilot",
+      imagePath: "/src/assets/placeholder.jpg",
+      imageAlt: "Speedway aircraft in flight, viewed from behind the pilots",
+      description:
+        "Pursue your career as a professional pilot with our structured Commercial Pilot Course.",
+      link: "/programs/commercial-pilot",
+    },
+    {
+      title: "Multi-Engine Rating",
+      imagePath: "/src/assets/placeholder.jpg",
+      imageAlt: "Speedway's multi-engine aircraft on runway viewed from the front",
+      description:
+        "Expand your capabilities with our Multi-Engine Rating course, designed to elevate your aviation skills.",
+      link: "/programs/multi-engine-rating",
+    },
+    {
+      title: "Certified Flight Instructor",
+      imagePath: "/src/assets/placeholder.jpg",
+      imageAlt: "Speedway instructor checking on plane instruments",
+      description:
+        "Share your passion for flying and inspire the next generation of pilots by becoming a Certified Flight Instructor.",
+      link: "/programs/certified-flight-instructor",
+    },
+    {
+      title: "Advanced Training & Add-Ons",
+      imagePath: "/src/assets/placeholder.jpg",
+      imageAlt: "Speedway aircraft taking off at sunset",
+      description:
+        "Looking to specialize? Explore Spin Training, BFRs, IPCs, and more advanced programs to enhance your skills.",
+      link: "/programs/advanced-trainings",
+    },
+  ],
+};
+
+export default programs;
