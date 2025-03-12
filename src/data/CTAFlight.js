073639b5bb3864783ef7051dc79b5be8c09b356d
lucascr <lcrodrigues1995@gmail.com>
2025-03-12 13:54:20 -0300

    Create CTAFlight.js

---
+++ src/data/CTAFlight.js
@@ -0,0 +1,15 @@
+const CTAFlight = {
+  imagePath: "/src/assets/placeholder.jpg",
+  imageAlt: "Speedway Flight Training student flying over Los Angeles",
+  headerH1: `<span class="text-primary-800">Take the First Step</br></span>With a Discovery Flight`,
+  paragraph:
+    "Experience the excitement of flying with a discovery flight at Speedway Flight Training. Whether you're pursuing a passion or preparing for a professional aviation career, this is your chance to take control and explore the skies over Los Angeles and beyond. Book your discovery flight today and start your journey into aviation!",
+  buttons: [
+    {
+      name: "Book Now",
+      link: "/discovery-flight",
+      primary: true,
+    },
+  ],
+};
+export default CTAFlight;
