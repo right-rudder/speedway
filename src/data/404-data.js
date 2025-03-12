80ce5cb849e22ce29ba4000c9ed96a2c4d960b22
lucascr <lcrodrigues1995@gmail.com>
2025-03-12 13:53:37 -0300

    Create 404-data.js

---
+++ src/data/404-data.js
@@ -0,0 +1,31 @@
+import { COMPANY_NAME, SITE_DESCRIPTION, KEYWORDS } from "../consts";
+
+const data404 = {
+  data: {
+    //SEO Header Stuff
+    pageTitle: `404 | ${COMPANY_NAME}`,
+    pageDescription: `This page shows a 404 error`,
+    pageKeywords: `404 error`,
+
+    header: {
+      imagePath: "/src/assets/placeholder.jpg",
+      imageAlt: `${COMPANY_NAME} plane taking off`,
+      headerH1: `well, this is AWKWARD!`,
+      paragraph: `It appears the page you're trying to access has already taken off, if you were trying to get some useful information please inform us so we can better accommodate your needs, otherwise just head on back to landing!`,
+      buttons: [
+        {
+          name: "Home Page",
+          link: "/",
+          primary: false,
+        },
+        {
+          name: "Contact us",
+          link: "/contact-us",
+          primary: false,
+        },
+      ],
+    },
+  },
+};
+
+export default data404;
