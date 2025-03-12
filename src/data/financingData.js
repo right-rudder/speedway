c06fee2f664b8fe8d6edaebf95753840a2ac06b8
lucascr <lcrodrigues1995@gmail.com>
2025-03-12 14:45:11 -0300

    Create financingData.js

---
+++ src/data/financingData.js
@@ -0,0 +1,9 @@
+const financingData = {
+  header: "Loan/financing options available",
+  button: {
+    text: "More Info",
+    link: "https://stratus.finance/apply/laflightacademy1303242/"
+  }
+}
+
+export default financingData;
