a1f57472a454411cd9236b4fe68473b432dea814
lucascr <lcrodrigues1995@gmail.com>
2025-03-12 13:54:12 -0300

    Create coursesIndex.js

---
+++ src/data/coursesIndex.js
@@ -0,0 +1,34 @@
+const coursesIndex = {
+  data: {
+    // SEO Header Stuff
+    pageTitle: "Training Courses | Speedway Flight Training",
+    pageDescription:
+      "Explore Speedway Flight Training’s comprehensive training programs, from Private Pilot License to advanced certifications like Instrument Rating, Multi-Engine, and Certified Flight Instructor. Tailored for aspiring pilots and professionals alike, with flexible schedules and expert instruction.",
+    pageKeywords:
+      "flight training courses Hampton, aviation certifications, private to commercial pilot, multi-engine rating training, CFI courses Georgia, aviation school programs",
+
+    // Top Header
+    header: {
+      stars: true,
+      imagePath: "/src/assets/placeholder.jpg",
+      imageAlt:
+        "Photograph of an Speedway Flight Training plane flying over mountains",
+      headerH1: `Pilot <br>PROGRAMS</span>`,
+      paragraph: `Join Speedway Flight Training and turn your aviation goals into reality. From your first discovery flight to advanced ratings, we’re here to guide you every step of the way.`,
+      buttons: [
+        {
+          name: "Enroll Today",
+          link: "/enroll-now",
+          primary: false,
+        },
+        {
+          name: "Program Guide",
+          link: "/path",
+          primary: false,
+        },
+      ],
+    },
+  },
+};
+
+export default coursesIndex;
