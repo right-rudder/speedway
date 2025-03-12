ba8f7dd3ab555dbb911052161925ed7a41910833
lucascr <lcrodrigues1995@gmail.com>
2025-03-12 14:45:05 -0300

    Create financingComp.js

---
+++ src/data/financingComp.js
@@ -0,0 +1,15 @@
+const financing = {
+  imagePath: "/src/assets/placeholder.jpg",
+  imageAlt: "Speedway Flight Training student pulling plane out of hangar",
+  headerH1: `<span class="w-full bg-clip-text text-transparent bg-gradient-to-br from-primary-800 to-primary-400 tracking-tight">Affordable&nbsp Paths&nbsp to&nbsp Your&nbsp Pilot&nbsp License&nbsp;</span>`,
+  paragraph:
+    "Pursuing your dream of becoming a pilot is now more accessible than ever with our flexible financing options. Whether you're just starting or advancing your certifications, we've partnered with trusted financial providers to help you achieve your goals. Explore our financing solutions and make your aviation ambitions a reality today!",
+  buttons: [
+    {
+      name: "Financing Page",
+      link: "/financing",
+      primary: true,
+    },
+  ],
+};
+export default financing;
