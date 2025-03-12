e7cb1b37e7a09e52b50b6bce63a360541d4f925a
lucascr <lcrodrigues1995@gmail.com>
2025-03-12 14:46:42 -0300

    Create homePage.js

---
+++ src/data/homePage.js
@@ -0,0 +1,17 @@
+import {
+  COMPANY_NAME,
+  SITE_DESCRIPTION,
+  KEYWORDS,
+} from "../consts";
+
+const homePage = {
+  data: {
+    //SEO Header Stuff
+    pageTitle: `Hampton Los Angeles Flight School for Pilot Training | ${COMPANY_NAME}`,
+    pageDescription:
+      `${SITE_DESCRIPTION}`,
+    pageKeywords:
+      `${KEYWORDS}`,
+  },
+};
+export default homePage;
