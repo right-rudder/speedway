e5c1e450c888e8db83745b3cc649dc3df4e0812e
lucascr <lcrodrigues1995@gmail.com>
2025-03-12 14:48:57 -0300

    Create programsSlug.js

---
+++ src/data/programsSlug.js
@@ -0,0 +1,17 @@
+import privatePilot from "./privatePilot";
+import instrumentRating from "./instrumentRating";
+import commercialPilot from "./commercialPilot";
+import multiEngine from "./multiEngine";
+import cfi from "./cfi";
+import advancedTraining from "./advancedTraining";
+
+const programs = [
+  privatePilot,
+  instrumentRating,
+  commercialPilot,
+  multiEngine,
+  cfi,
+  advancedTraining,
+];
+
+export default programs;
