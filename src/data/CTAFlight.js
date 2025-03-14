309b11a0beb21c6ad6bc0437f638e8b988501b94
lucascr <lcrodrigues1995@gmail.com>
2025-03-14 11:52:13 -0300

    Update CTAFlight.js

--- src/data/CTAFlight.js
+++ src/data/CTAFlight.js
@@ -1,7 +1,7 @@
 const CTAFlight = {
   imagePath: "/src/assets/inside-cockpit-flight-training-speedway-ga.jpg",
   imageAlt: "Speedway Flight Training student flying over Atlanta Motor Speedway",
-  headerH1: `<span class="text-primary-800">Take the First Step</br></span>With a Discovery Flight`,
+  headerH1: `Take the First Step</br>With a <span class="text-primary-700">Discovery Flight</span>`,
   paragraph:
     "Experience the thrill of flying with a Discovery Flight at Speedway Flight Training. Whether you're exploring aviation as a passion or considering a career in flight, this hands-on experience lets you take the controls under the guidance of a certified instructor. Fly over <strong>Atlanta Motor Speedway, Stone Mountain, and the Atlanta skyline</strong> as you embark on your aviation journey. Book your Discovery Flight today!",
   buttons: [
