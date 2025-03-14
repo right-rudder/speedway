3825a64d9cbaf71b2943ca922dd682d37cb240fc
lucascr <lcrodrigues1995@gmail.com>
2025-03-14 11:52:16 -0300

    Update financingComp.js

--- src/data/financingComp.js
+++ src/data/financingComp.js
@@ -1,7 +1,7 @@
 const financing = {
   imagePath: "/src/assets/student-pilot-training-in-cockpit-speedway-ga.jpg",
   imageAlt: "Speedway Flight Training student pulling plane out of hangar",
-  headerH1: `Affordable&nbsp Paths&nbsp to&nbsp Your&nbsp Pilot&nbsp License&nbsp`,
+  headerH1: `<span class="text-primary-700">Affordable</span>&nbsp Paths&nbsp to&nbsp Your&nbsp Pilot&nbsp License&nbsp`,
   paragraph:
     "Pursuing your dream of becoming a pilot is now more accessible than ever with our flexible financing options. Whether you're just starting or advancing your certifications, we've partnered with trusted financial providers to help you achieve your goals. Explore our financing solutions and make your aviation ambitions a reality today!",
   buttons: [
