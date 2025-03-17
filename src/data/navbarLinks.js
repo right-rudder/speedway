1eec5da47cbb10fbf62dca1cff37730f67406e56
lucascr <lcrodrigues1995@gmail.com>
2025-03-17 17:52:27 -0300

    Update navbarLinks.js

--- src/data/navbarLinks.js
+++ src/data/navbarLinks.js
@@ -6,10 +6,35 @@ export const navbarLinks = [
     submenu: [],
   },
   {
-    name: "Fleet",
-    link: "/fleet",
+    name: "Programs",
+    link: "/programs",
     mobileLink: true,
-    submenu: [],
+    submenu: [
+      {
+        name: "Private Pilot",
+        link: "/programs/private-pilot",
+      },
+      {
+        name: "Instrument Rating",
+        link: "/programs/instrument-rating",
+      },
+      {
+        name: "Commercial Pilot",
+        link: "/programs/commercial-pilot",
+      },
+      {
+        name: "Multi-Engine Rating",
+        link: "/programs/multi-engine-rating",
+      },
+      {
+        name: "Flight Instructor",
+        link: "/programs/certified-flight-instructor",
+      },
+      {
+        name: "Advanced Training",
+        link: "/programs/advanced-trainings",
+      },
+    ],
   },
   {
     name: "More Info",
