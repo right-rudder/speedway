185c75f2fa36309386a215cea6d405d4a6f1447c
lucascr <lcrodrigues1995@gmail.com>
2025-03-24 15:33:44 -0300

    Create navbarLinks2.js

---
+++ src/data/navbarLinks2.js
@@ -0,0 +1,25 @@
+export const navbarLinks2 = [
+  {
+    name: "More Info",
+    link: "/speedway",
+    mobileLink: false,
+    submenu: [
+      {
+        name: "Fleet",
+        link: "/fleet",
+      },
+      {
+        name: "About Us",
+        link: "/about-us",
+      },
+      {
+        name: "Gallery",
+        link: "/gallery",
+      },
+      {
+        name: "Blog",
+        link: "/blog",
+      },
+    ],
+  },
+];
