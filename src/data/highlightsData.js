67dc0f2252eeaedcd226b7896c93610fc8b1b84b
lucascr <lcrodrigues1995@gmail.com>
2025-03-24 15:33:37 -0300

    Create highlightsData.js

---
+++ src/data/highlightsData.js
@@ -0,0 +1,28 @@
+const highlights = {
+  heading: "What Makes Speedway Extraordinary?",
+  items: [
+    {
+      title: "Student Centered",
+      description:
+        "You're more than just flight hours. At Speedway, we genuinely care about your success and growth as a pilot. We’re here to help you every step of the way.",
+      imagePath: "/src/assets/handshake.webp",
+      imageAlt: "Handshake icon representing personal support",
+    },
+    {
+      title: "Family Environment",
+      description:
+        "We’re a tight-knit aviation community that encourages, celebrates, and supports each other. You’re not just a student — you’re part of the Speedway family.",
+      imagePath: "/src/assets/family.webp",
+      imageAlt: "Family icon representing community atmosphere",
+    },
+    {
+      title: "Affordable",
+      description:
+        "Speedway offers transparent pricing and cost-effective packages to help make professional flight training accessible to more aspiring aviators.",
+      imagePath: "/src/assets/affordable.webp",
+      imageAlt: "Dollar sign icon representing affordability",
+    },
+  ],
+};
+
+export default highlights;
