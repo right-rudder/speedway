c5256e066c328e933677982fe80830e5b4d123f3
lucascr <lcrodrigues1995@gmail.com>
2025-03-25 17:32:08 -0300

    Create testimonialsSwiper.js

---
+++ src/data/testimonialsSwiper.js
@@ -0,0 +1,40 @@
+const testimonialsSwiper = {
+  upperHeading: "Testimonials",
+  heading: "What are our students saying?",
+  description:
+    "Learn about our students' experiences and what they have to say about Speedway's training programs.",
+  cards: [
+    {
+      quoteText: `“iFly es la familia que me vió nacer como piloto aviador profesional y
+      me ayudó a cumplir uno de mis más grandes sueños, es para mí un placer
+      y un orgullo haberme formado en esta gran familia.”`,
+      citeText: `- Roberto Novelo Cicero (Piloto aviador)`,
+      imagePath: "/src/assets/speedway-flight-training-hampton-georgia-logo-gray.webp",
+      imageAlt:
+        "Speedway logo in gray",
+    },
+    {
+      quoteText: `“Estudiar en iFly fue una de las mejores decisiones que he tomado en mi vida y una gran experiencia. Si me preguntaran de nuevo la elegiría sin pensarlo, ya que me permitió sentar bases sólidas en mi formación como piloto desde el punto de vista académico y personal. Cuentan con instalaciones de primer nivel, instructores altamente capacitados, aviones con excelente mantenimiento y una gran diversidad de características que hoy en día me ayudan a realizar mi trabajo de forma profesional y segura.`,
+      citeText: `- Eduardo Luján Aldaz (Piloto aviador)`,
+      imagePath: "/src/assets/speedway-flight-training-hampton-georgia-logo-gray.webp",
+      imageAlt:
+        "Speedway logo in gray",
+    },
+    {
+      quoteText: `“Estudié mi carrera de piloto comercial en iFly, y puedo decir que tienen excelentes instalaciones, aeronaves, simuladores. Tienen muy buena calidad en la enseñanza con muy buenos instructores.”`,
+      citeText: `- Rodrigo Minor (Piloto aviador)`,
+      imagePath: "/src/assets/speedway-flight-training-hampton-georgia-logo-gray.webp",
+      imageAlt:
+        "Speedway logo in gray",
+    },
+    {
+      quoteText: `“Haber estudiado en iFly sin duda alguna fue una excelente decisión, iFly es una institución que te provee las mejores herramientas para desarrollarte profesionalmente como piloto aviador comercial, hace que te rodees de gente con gran experiencia y completamente inmersa en el medio aeronáutico. Los instructores de la escuela además de ser excelentes personas, son gente completamente apasionada por este hermoso medio y mas importante personas con ganas de promover y mejorar el medio aeronáutico en México. Con toda seguridad puedo decir que es la mejor escuela de aviación en México.”`,
+      citeText: `- Luis Miguel Pérez Villalón (Piloto aviador)`,
+      imagePath: "/src/assets/speedway-flight-training-hampton-georgia-logo-gray.webp",
+      imageAlt:
+        "Speedway logo in gray",
+    },
+  ],
+};
+
+export default testimonialsSwiper;
