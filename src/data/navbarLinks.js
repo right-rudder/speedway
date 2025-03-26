001409af6eb65fc4fa22fbf34eddf884fd78f9ab
lucascr <lcrodrigues1995@gmail.com>
2025-03-26 04:06:08 -0300

    Update navbarLinks.js

--- src/data/navbarLinks.js
+++ src/data/navbarLinks.js
@@ -1,53 +1,53 @@
 export const navbarLinks = [
   {
     name: "Fast-Track",
-    link: "/fast-track-program",
+    link: "/fast-track-program#top",
     mobileLink: true,
     submenu: [],
   },
   {
     name: "Programs",
-    link: "/programs",
+    link: "/programs#top",
     mobileLink: true,
     submenu: [
       {
         name: "Private Pilot",
-        link: "/programs/private-pilot",
+        link: "/programs/private-pilot#top",
       },
       {
         name: "Instrument Rating",
-        link: "/programs/instrument-rating",
+        link: "/programs/instrument-rating#top",
       },
       {
         name: "Commercial Pilot",
-        link: "/programs/commercial-pilot",
+        link: "/programs/commercial-pilot#top",
       },
       {
         name: "Flight Instructor",
-        link: "/programs/certified-flight-instructor",
+        link: "/programs/certified-flight-instructor#top",
       },
       {
         name: "CFI - Instrument",
-        link: "/programs/certified-flight-instructor-instrument-cfii",
+        link: "/programs/certified-flight-instructor-instrument-cfii#top",
       },
     ],
   },
   {
     name: "About Us",
-    link: "/speedway",
+    link: "/speedway#top",
     mobileLink: false,
     submenu: [
       {
         name: "Fleet",
-        link: "/speedway/fleet",
+        link: "/speedway/fleet#top",
       },
       {
         name: "Crew",
-        link: "/speedway/crew",
+        link: "/speedway/crew#top",
       },
       {
         name: "Blog",
-        link: "/blog",
+        link: "/blog#top",
       },
     ],
   },
