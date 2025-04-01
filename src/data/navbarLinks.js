828da4d09d8a5a73b9a6ac06c193f64df8dcc2a6
lucascr <lcrodrigues1995@gmail.com>
2025-04-01 18:09:23 -0300

    Update navbarLinks.js

--- src/data/navbarLinks.js
+++ src/data/navbarLinks.js
@@ -61,6 +61,10 @@ export const navbarLinks = [
         name: "Blog",
         link: "/blog#top",
       },
+      {
+        name: "Financing",
+        link: "/speedway/financing#top",
+      },
     ],
   },
 ];
