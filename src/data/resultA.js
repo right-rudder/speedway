2604252f6408632a6058972ab6acd4a22c6bee1e
lucascr <lcrodrigues1995@gmail.com>
2025-04-01 17:31:49 -0300

    Update resultA.js

--- src/data/resultA.js
+++ src/data/resultA.js
@@ -14,7 +14,7 @@ const resultA = {
 
     header: {
       imagePath:
-        "/src/assets/placeholder.jpg",
+        "/src/assets/placeholder.webp",
       imageAlt: `${COMPANY_NAME} CFI watching as student prepares to take first solo flight`,
       headerH1: "Sky's the Limit!",
       paragraph:
